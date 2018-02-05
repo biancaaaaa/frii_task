bc3f7f2fc59dbda68f24c68b863e660a26a304cf
EB820G2\Bianca <bianca.hoellmueller@gmx.net>
2018-02-05 13:09:17 +0100

    application proj 20180205

--- js/calc.js
+++ js/calc.js
@@ -1,15 +1,24 @@
+//constant for the price of the box
 const TRIPRICE = 99;
 
 $(document).ready(function($) {
     let quantityInput = $("#quantity");
+    //sets value to 1 when window is loaded
     quantityInput.val(1);
     refreshPrice();
 
+    //refreshes the price on keyup
     quantityInput.on("keyup", function() {
        refreshPrice();
     });
+
+    //refreshes the price on click (when using the counter on number input)
+    quantityInput.on("click", function() {
+        refreshPrice();
+    });
 });
 
+//reads the value of the input field and sets the current price
 function refreshPrice() {
     let price = $("#price");
     price.empty();
